724d82fdd127e312ec09d78d2f63668fe373fdd3
Mihail-K86 <136430312+Mihail-K86@users.noreply.github.com>
2023-08-14 16:09:47 +0300

    Add files via upload

---
+++ simple_signup/sign/views.py
@@ -0,0 +1,62 @@
+
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+from .models import BaseRegisterForm
+
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render, reverse, redirect
+from django.views import View
+from django.core.mail import send_mail
+from datetime import datetime
+
+from .models import Appointment
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+class BaseRegisterView(CreateView):
+    model = User
+    form_class = BaseRegisterForm
+    success_url = '/'
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    premium_group = Group.objects.get(name='premium')
+    if not request.user.groups.filter(name='premium').exists():
+        premium_group.user_set.add(user)
+    return redirect('/')
+
+
+class AppointmentView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'make_appointment.html', {})
+
+    def post(self, request, *args, **kwargs):
+        appointment = Appointment(
+            date=datetime.strptime(request.POST['date'], '%Y-%m-%d'),
+            client_name=request.POST['client_name'],
+            message=request.POST['message'],
+        )
+        appointment.save()
+
+        # получаем наш html
+        html_content = render_to_string(
+            'appointment_created.html',
+            {
+                'appointment': appointment,
+            }
+        )
+
+        # в конструкторе уже знакомые нам параметры, да? Называются правда немного по-другому, но суть та же.
+        msg = EmailMultiAlternatives(
+            subject=f'{appointment.client_name} {appointment.date.strftime("%Y-%M-%d")}',
+            body=appointment.message,  # это то же, что и message
+            from_email='peterbadson@yandex.ru',
+            to=['skavik46111@gmail.com'],  # это то же, что и recipients_list
+        )
+        msg.attach_alternative(html_content, "text/html")  # добавляем html
+        msg.send()  # отсылаем
+
+        return redirect('appointments:make_appointment')
---
+++ simple_signup/simple_signup/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('protect.urls')),
+    path('sign/', include('sign.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('appointments/', include(('appointment.urls', 'appointments'), namespace='appointments')),
+]

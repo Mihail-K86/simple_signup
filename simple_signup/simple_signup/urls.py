3a3434145a9c72c19995ce94393197cca9b7b59a
Mihail-K86 <136430312+Mihail-K86@users.noreply.github.com>
2023-08-17 17:33:10 +0300

    Delete simple_signup directory

--- simple_signup/simple_signup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('protect.urls')),
-    path('sign/', include('sign.urls')),
-    path('accounts/', include('allauth.urls')),
-]
